d6d3b5f6227b7cb39719de17f569b97eb3e3e685
rifqi kautsar romadoni <rifqikautsarromadoni@gmail.com>
2025-07-18 11:05:26 +0700

    update version

---
+++ koneksi.php
@@ -0,0 +1,9 @@
+<?php
+
+$_SERVER = "localhost";
+$username = "root";
+$password = "";
+$database = "db_toko_rifqi_xiiisija";
+$koneksi = mysqli_connect($_SERVER, $username, $password, $database)
+
+    ?>
